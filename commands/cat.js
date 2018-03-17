c5ee35961f8fca2e54d4640cbe72775136d5a020
Ori Kenig <orikenig@gmail.com>
2018-03-17 16:27:47 +0200

    test

--- commands/cat.js
+++ commands/cat.js
@@ -3,8 +3,7 @@ const superagent = require("superagent");
 
 module.exports.run = async (bot,message,args) => {
 	
-	let {body} = await superagent
-	.get(`http://aws.random.cat/meow`).then(function(res) {
+	superagent.get(`http://aws.random.cat/meow`).then(function(res) {
 		console.log(res.text);
 		console.log(res.text.substring(9, res.text.length-2));
 		
@@ -13,7 +12,7 @@ module.exports.run = async (bot,message,args) => {
 		.setImage(res.text.substring(9, res.text.length-2));
 	
 		message.channel.send(catembed);
-    });;
+    });
 }
 
 module.exports.help = {
