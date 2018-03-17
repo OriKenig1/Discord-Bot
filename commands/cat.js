e58bb316997776ca1f3ba8f2d795ee387b4a7bd0
Ori Kenig <orikenig@gmail.com>
2018-03-17 17:00:16 +0200

    test

--- commands/cat.js
+++ commands/cat.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot,message,args) => {
 	
 	superagent.get(`http://aws.random.cat/meow`).then(function(res) {
 		console.log(res.text);
-		var str = replaceAll(res.text.substring(9, res.text.length-2), "/", "");
+		var str = res.text.substring(9, res.text.length-2).split("/").join("");
 		console.log(str);
 		
 		let catembed = new Discord.RichEmbed()
@@ -14,10 +14,6 @@ module.exports.run = async (bot,message,args) => {
 	
 		message.channel.send(catembed);
     });
-}
-
-function replaceAll(str, find, replace) {
-    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 }
 
 module.exports.help = {
