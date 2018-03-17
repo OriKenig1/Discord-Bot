1b9f5389c1d988c1d4f14b1f7a28c39149792cae
Ori Kenig <orikenig@gmail.com>
2018-03-17 15:38:42 +0200

    test

--- commands/cat.js
+++ commands/cat.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot,message,args) => {
 	let {body} = await superagent
 	.get(`http://aws.random.cat/meow`).then(function(res) {
 		console.log(res);
-		console.log(res.substring(10, res.length-2);
+		console.log(res.substring(10, res.length-2));
     });;
 	
 	/*
