eb70ec7a9be263d8e5ee788f0a491595cef6fedc
Ori Kenig <orikenig@gmail.com>
2018-03-17 17:05:16 +0200

    test

--- commands/cat.js
+++ commands/cat.js
@@ -8,10 +8,11 @@ module.exports.run = async (bot,message,args) => {
 		var str = res.text.substring(9, res.text.length-2).split("/").join("");
 		console.log(str);
 		
-		message.channel.send("s", {
-		file: str 
-		});
-		
+		let catembed = new Discord.RichEmbed()
+		.setColor("#ff9900")
+		.setUrl(str);
+	
+		message.channel.send(catembed);
     });
 }
 
