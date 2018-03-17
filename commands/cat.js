1c56bf90cf03be08661b825c608291e54c666a6b
Ori Kenig <orikenig@gmail.com>
2018-03-17 17:31:28 +0200

    test

--- commands/cat.js
+++ commands/cat.js
@@ -8,8 +8,8 @@ module.exports.run = async (bot,message,args) => {
 		console.log(res.text);
 		var str = res.text.substring(9, res.text.length-2);
 		str = str.replace("s", "");
-		str = str.split("\"");
-		str = str.join("");
+		str = str.split("\/");
+		str = str.join("/");
 		console.log(str);
 		
 		let catembed = new Discord.RichEmbed()
