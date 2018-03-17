c5f9373be01f3b17a2c9a6feca1fe62719b114a7
Ori Kenig <orikenig@gmail.com>
2018-03-17 17:23:02 +0200

    test

--- commands/cat.js
+++ commands/cat.js
@@ -6,8 +6,17 @@ module.exports.run = async (bot,message,args) => {
 	superagent.get(`http://aws.random.cat/meow`).then(function(res) {
 		
 		console.log(res.text);
-		var str = res.text.substring(9, res.text.length-2).split("/").join("");
+		var str = res.text.substring(9, res.text.length-2).replace("s", "").split("\").join("");
 		console.log(str);
+		
+		let catembed = new Discord.RichEmbed()
+		.setColor("#ff9900")
+		.setImage(str);
+	
+		message.channel.send(catembed);
+		
+		/*
+
 		
 		let {body} = superagent.get(str).then(function(res) {
 			
@@ -20,6 +29,7 @@ module.exports.run = async (bot,message,args) => {
 	
 			message.channel.send(catembed);
 		});
+		*/
 	});
 }
 
