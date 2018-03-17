1dbc63d41fe165fbc8f01d481b28b846923c0036
Ori Kenig <orikenig@gmail.com>
2018-03-17 15:37:37 +0200

    test

--- commands/cat.js
+++ commands/cat.js
@@ -2,18 +2,17 @@ const Discord = require("discord.js");
 const superagent = require("superagent");
 
 module.exports.run = async (bot,message,args) => {
-	/*
+	
 	let {body} = await superagent
-	.get(`http://aws.random.cat/meow`);
+	.get(`http://aws.random.cat/meow`).then(function(res) {
+		console.log(res);
+		console.log(res.substring(10, res.length-2);
+    });;
 	
 	/*
-	console.log(body.get("file"));
-	
 	let dogembed = new Discord.RichEmbed()
 	.setColor("#ff9900")
 	.setImage(body);
-	
-	
 	
 	message.channel.send(dogembed);
 	*/
